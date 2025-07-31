89e507d92fe72fb8985bca12f0ae91b660d24c1c
Julian Lopez Padua <julian.lopez@rcpo.com.ar>
2025-07-30 21:16:50 -0300

    renderizado de ejercicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EjerciciosServiceService } from './services/ejercicios-service.service';
+import { DiaGym, Ejercicios } from './models/ArrayGym.model';
+import { CardEjercicioComponent } from './components/card-ejercicio/card-ejercicio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardEjercicioComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app-gym';
+  ejerciciosService: DiaGym[];
+  Pecho: Ejercicios[] = [];
+  Triceps: Ejercicios[] = [];
+  Espalda: Ejercicios[] = [];
+  Biceps: Ejercicios[] = [];
+  Hombros: Ejercicios[] = [];
+  Piernas: Ejercicios[] = [];
+  Abdominales: Ejercicios[] = [];
+  constructor($ejerciciosService: EjerciciosServiceService) {
+    this.ejerciciosService = $ejerciciosService.arrayGym;
+    this.Pecho = $ejerciciosService.Pecho;
+    this.Abdominales = $ejerciciosService.Abdominales;
+    this.Biceps = $ejerciciosService.Biceps;
+    this.Triceps = $ejerciciosService.Tríceps;
+    this.Espalda = $ejerciciosService.Espalda;
+    this.Hombros = $ejerciciosService.Hombros;
+    this.Piernas = $ejerciciosService.Piernas;
+  }
 }
